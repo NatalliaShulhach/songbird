1574b72efba45010c5e728ecd351d202d4ac5b13
NatalliaShulhach <55178199+NatalliaShulhach@users.noreply.github.com>
2020-08-30 22:16:34 +0300

    init: init project

---
+++ build/precache-manifest.41ef1af7026a480bfb6bc8cb43eda1e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a282c5cb22e16f5d303c71291a93ec8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d256c8dd4208839d9839",
+    "url": "/static/css/main.dd5f67dc.chunk.css"
+  },
+  {
+    "revision": "0027973ef599e332f051",
+    "url": "/static/js/2.ec82d39b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ec82d39b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d256c8dd4208839d9839",
+    "url": "/static/js/main.b299fcec.chunk.js"
+  },
+  {
+    "revision": "dc9173b7e8edec60da3a",
+    "url": "/static/js/runtime-main.3ffde9c1.js"
+  },
+  {
+    "revision": "c59c1ab15f452eea5a38d496a08857e7",
+    "url": "/static/media/correct_answer.c59c1ab1.mp3"
+  },
+  {
+    "revision": "cbd6334454e32934a594c28e4f4a9133",
+    "url": "/static/media/wrong_answer.cbd63344.mp3"
+  }
+]);
